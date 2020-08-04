bb7fe095e5cb6fd4add0d64b2022b311bdfbfb2c
Reda Boutaa <reda.boutaa@aperam.com>
2020-08-04 12:50:48 +0200

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
